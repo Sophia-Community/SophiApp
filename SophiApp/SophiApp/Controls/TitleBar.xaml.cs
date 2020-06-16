87a73280126ac1e174d47946a3ecc0ef75e4b0c0
oZ-Zo <50652175+oz-zo@users.noreply.github.com>
2020-06-16 18:07:51 +0300

    Create TitleBar control

---
+++ SophiApp/SophiApp/Controls/TitleBar.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace SophiApp.AppControls
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowTitleBar.xaml
+    /// </summary>
+    public partial class TitleBar : UserControl
+    {
+        public TitleBar()
+        {
+            InitializeComponent();
+        }
+
+        private void TitleBarButtonMinimize_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;            
+        }
+
+        private void TitleBarButtonMaximize_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = Application.Current.MainWindow.WindowState == WindowState.Normal
+                ? WindowState.Maximized 
+                : WindowState.Normal;            
+
+            TitleBarButtonMaximizeIcon.Data = Application.Current.MainWindow.WindowState == WindowState.Maximized
+                ? Application.Current.TryFindResource("MainWindow.TitleIcon.Collapse") as Geometry
+                : Application.Current.TryFindResource("MainWindow.TitleIcon.Maximize") as Geometry;
+        }
+
+        private void TitleBarButtonClose_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.MainWindow.Close();
+        }        
+    }
+}
