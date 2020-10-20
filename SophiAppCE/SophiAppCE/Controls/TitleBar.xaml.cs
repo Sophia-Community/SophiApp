488b86345157b3497f356bda71e5e3b93e59b30d
oZ-Zo <50652175+oz-zo@users.noreply.github.com>
2020-10-20 17:14:11 +0300

    New "Fancy" app theme

---
+++ SophiAppCE/SophiAppCE/Controls/TitleBar.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SophiAppCE.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для TitleBar.xaml
+    /// </summary>
+    public partial class TitleBar : UserControl
+    {
+        public TitleBar()
+        {
+            InitializeComponent();
+        }
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(TitleBar), new PropertyMetadata(default(string)));
+
+        private void TittleBarButtonClose_Click(object sender, RoutedEventArgs e) => Application.Current.MainWindow.Close();
+
+        private void TitleBarButtonMinimize_Click(object sender, RoutedEventArgs e) => Application.Current.MainWindow.WindowState = WindowState.Minimized;
+
+        private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                Application.Current.MainWindow.DragMove();
+        }
+    }
+}
---
+++ SophiAppCE/SophiAppCE/Controls/TitleBarButtonMinimize.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SophiAppCE.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для TitleBarButtonMinimize.xaml
+    /// </summary>
+    public partial class TitleBarButtonMinimize : UserControl
+    {
+        public TitleBarButtonMinimize()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly RoutedEvent ClickEvent = EventManager.RegisterRoutedEvent("Click", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(TitleBarButtonMinimize));
+
+        public event RoutedEventHandler Click
+        {
+            add { AddHandler(ClickEvent, value); }
+            remove { RemoveHandler(ClickEvent, value); }
+        }
+
+        private void TitleBarButtonMinimize_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            RaiseEvent(new RoutedEventArgs(ClickEvent));
+        }
+
+        public Brush Hover
+        {
+            get { return (Brush)GetValue(HoverProperty); }
+            set { SetValue(HoverProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Hover.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty HoverProperty =
+            DependencyProperty.Register("Hover", typeof(Brush), typeof(TitleBarButtonMinimize), new PropertyMetadata(default(Brush)));
+
+        public Geometry Icon
+        {
+            get { return (Geometry)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Icon.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(Geometry), typeof(TitleBarButtonMinimize), new PropertyMetadata(default(Geometry)));
+
+        public Brush IconBrush
+        {
+            get { return (Brush)GetValue(IconBrushProperty); }
+            set { SetValue(IconBrushProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IconBrush.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IconBrushProperty =
+            DependencyProperty.Register("IconBrush", typeof(Brush), typeof(TitleBarButtonMinimize), new PropertyMetadata(default(Brush)));
+    }
+}
