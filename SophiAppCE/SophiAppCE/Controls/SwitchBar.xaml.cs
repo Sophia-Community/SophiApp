09c6ee14a808cef603aa798505ddf8a10f748f61
oZ-Zo <50652175+oz-zo@users.noreply.github.com>
2020-10-26 17:59:01 +0300

    Refactoring controls templates :recycle:

--- SophiAppCE/SophiAppCE/Controls/SwitchBar.xaml.cs
+++ SophiAppCE/SophiAppCE/Controls/SwitchBar.xaml.cs
@@ -24,5 +24,25 @@ namespace SophiAppCE.Controls
         {
             InitializeComponent();
         }
+
+        public string Header
+        {
+            get { return (string)GetValue(HeaderProperty); }
+            set { SetValue(HeaderProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Header.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty HeaderProperty =
+            DependencyProperty.Register("Header", typeof(string), typeof(SwitchBar), new PropertyMetadata(default(string)));
+
+        public string Description
+        {
+            get { return (string)GetValue(DescriptionProperty); }
+            set { SetValue(DescriptionProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Description.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty DescriptionProperty =
+            DependencyProperty.Register("Description", typeof(string), typeof(SwitchBar), new PropertyMetadata(default(string)));
     }
 }
