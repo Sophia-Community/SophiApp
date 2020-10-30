8e4a4ce236f7f11c40b1ff5fa6c5c53685ec9418
oZ-Zo <50652175+oz-zo@users.noreply.github.com>
2020-10-30 16:46:47 +0300

    Added select all control

---
+++ SophiAppCE/SophiAppCE/Controls/Switch.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SophiAppCE.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для Switch.xaml
+    /// </summary>
+    public partial class Switch : UserControl
+    {
+        public Switch()
+        {
+            InitializeComponent();
+        }
+        
+        public bool State
+        {
+            get { return (bool)GetValue(StateProperty); }
+            set { SetValue(StateProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for State.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty StateProperty =
+            DependencyProperty.Register("State", typeof(bool), typeof(Switch), new PropertyMetadata(null));
+    }
+}
---
+++ SophiAppCE/SophiAppCE/Controls/TextSwitchBar.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SophiAppCE.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для TextSwitchBar.xaml
+    /// </summary>
+    public partial class TextSwitchBar : UserControl
+    {
+        public TextSwitchBar()
+        {
+            InitializeComponent();
+        }
+
+        public string TextOn
+        {
+            get { return (string)GetValue(TextOnProperty); }
+            set { SetValue(TextOnProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for TextOn.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextOnProperty =
+            DependencyProperty.Register("TextOn", typeof(string), typeof(TextSwitchBar), new PropertyMetadata(default(string)));
+
+        public string TextOff
+        {
+            get { return (string)GetValue(TextOffProperty); }
+            set { SetValue(TextOffProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for TextOff.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextOffProperty =
+            DependencyProperty.Register("TextOff", typeof(string), typeof(TextSwitchBar), new PropertyMetadata(default(string)));
+
+        public bool State
+        {
+            get { return (bool)GetValue(StateProperty); }
+            set { SetValue(StateProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for State.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty StateProperty =
+            DependencyProperty.Register("State", typeof(bool), typeof(TextSwitchBar), new PropertyMetadata(null));
+
+
+
+    }
+}
