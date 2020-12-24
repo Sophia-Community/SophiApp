23576c7ff9fa12077ebfe1061476ef1cd2daec88
oZ-Zo <50652175+oz-zo@users.noreply.github.com>
2020-12-24 16:40:43 +0300

    Added Setting page :page_facing_up:

---
+++ SophiAppCE/SophiAppCE/Controls/LinkBar.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SophiAppCE.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для LinkBar.xaml
+    /// </summary>
+    public partial class LinkBar : UserControl
+    {
+        public LinkBar()
+        {
+            InitializeComponent();
+        }
+
+        public double IconHeight
+        {
+            get { return (double)GetValue(IconHeightProperty); }
+            set { SetValue(IconHeightProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IconHeight.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IconHeightProperty =
+            DependencyProperty.Register("IconHeight", typeof(double), typeof(LinkBar), new PropertyMetadata(default(double)));
+
+        public double IconWidth
+        {
+            get { return (double)GetValue(IconWidthProperty); }
+            set { SetValue(IconWidthProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IconWidth.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IconWidthProperty =
+            DependencyProperty.Register("IconWidth", typeof(double), typeof(LinkBar), new PropertyMetadata(default(double)));
+
+        public Geometry Icon
+        {
+            get { return (Geometry)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Icon.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(Geometry), typeof(LinkBar), new PropertyMetadata(default(Geometry)));
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(LinkBar), new PropertyMetadata(default(string)));
+
+
+        public string Link
+        {
+            get { return (string)GetValue(LinkProperty); }
+            set { SetValue(LinkProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Link.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LinkProperty =
+            DependencyProperty.Register("Link", typeof(string), typeof(LinkBar), new PropertyMetadata("https://www.yandex.ru"));
+
+        private void LinkBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = Link;
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+            process.Start();
+        }
+    }
+}
