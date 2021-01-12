b437a64a539c5bce7e9dca49bd70486d92159f1d
oZ-Zo <50652175+oz-zo@users.noreply.github.com>
2021-01-12 17:57:14 +0300

    Added MVVM implementation

---
+++ SophiAppCE/SophiAppCE/Helpers/AppHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SophiAppCE.Helpers
+{
+    internal static class AppHelper
+    {
+        internal static string GetName() => "SophiApp Community Edition";
+
+        internal static string GetVersion() => Assembly.GetExecutingAssembly().GetName().Version.ToString().Substring(0, 5);
+
+        internal static string GetFullName() => $"{GetName()} {GetVersion()}";
+        
+    }
+}
---
+++ SophiAppCE/SophiAppCE/ViewModels/MainVM.cs
@@ -0,0 +1,28 @@
+﻿using SophiAppCE.Helpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SophiAppCE.ViewModels
+{
+    class MainVM : INotifyPropertyChanged
+    {
+        public MainVM()
+        {
+            
+        }
+
+        public string AppTitle { get => AppHelper.GetFullName(); }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyChanged)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyChanged));
+        }
+    }
+}
