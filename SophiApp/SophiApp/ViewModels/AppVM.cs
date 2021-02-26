262fc2f2ada884e6dc4d42ad9af6b6fb6d00b5e2
Inestic <50652175+oz-zo@users.noreply.github.com>
2021-02-26 16:37:24 +0300

    UI general update :zap:

---
+++ SophiApp/SophiApp/ViewModels/AppVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SophiApp.ViewModels
+{
+    class AppVM : INotifyPropertyChanged
+    {        
+        public string AppName => Application.Current.FindResource("CONST.AppName") as string;
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyChanged) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyChanged));
+    }
+}
