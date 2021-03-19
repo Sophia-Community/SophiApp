50d219a8471cc30e309825b49dfef09886236d0a
Inestic <50652175+oz-zo@users.noreply.github.com>
2021-03-19 12:33:25 +0300

    Added checkbox control :ballot_box_with_check:

---
+++ SophiApp/SophiApp/Controls/CheckBox.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SophiApp.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для CheckBox.xaml
+    /// </summary>
+    public partial class CheckBox : UserControl
+    {
+        public CheckBox()
+        {
+            InitializeComponent();
+        }
+
+        public bool IsChecked
+        {
+            get { return (bool)GetValue(IsCheckedProperty); }
+            set { SetValue(IsCheckedProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsChecked.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsCheckedProperty =
+            DependencyProperty.Register("IsChecked", typeof(bool), typeof(CheckBox), new PropertyMetadata(default(bool)));
+
+        private void CheckBox_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) => IsChecked = !IsChecked;
+        
+    }
+}
--- SophiApp/SophiApp/Controls/SwitchBar.xaml.cs
+++ SophiApp/SophiApp/Controls/SwitchBar.xaml.cs
@@ -9,18 +9,30 @@ namespace SophiApp.Controls
     public partial class SwitchBar : UserControl
     {
         // Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty TextProperty =
-            DependencyProperty.Register("Text", typeof(string), typeof(SwitchBar), new PropertyMetadata(default(string)));
+        public static readonly DependencyProperty HeaderProperty =
+            DependencyProperty.Register("Header", typeof(string), typeof(SwitchBar), new PropertyMetadata(default(string)));
 
         public SwitchBar()
         {
             InitializeComponent();
         }
 
-        public string Text
+        public string Header
         {
-            get { return (string)GetValue(TextProperty); }
-            set { SetValue(TextProperty, value); }
+            get { return (string)GetValue(HeaderProperty); }
+            set { SetValue(HeaderProperty, value); }
         }
+
+        public string Description
+        {
+            get { return (string)GetValue(DescriptionProperty); }
+            set { SetValue(DescriptionProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Description.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty DescriptionProperty =
+            DependencyProperty.Register("Description", typeof(string), typeof(SwitchBar), new PropertyMetadata(default(string)));
+
+
     }
 }
