a5b6521c4ea3c390010e7786a87e887eab421604
Inestic <50652175+oz-zo@users.noreply.github.com>
2021-03-20 07:49:14 +0300

    Update window layout :straight_ruler:

---
+++ SophiApp/SophiApp/Views/PrivacyView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SophiApp.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для PrivacyView.xaml
+    /// </summary>
+    public partial class PrivacyView : UserControl
+    {
+        public PrivacyView()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl_MouseEnter(object sender, MouseEventArgs e)
+        {
+
+        }
+    }
+}
