321acc19110ec10a0c18e178756daf2b7e7767ac
Inestic <50652175+Inestic@users.noreply.github.com>
2021-04-05 16:44:39 +0300

    Toggle button refactoring :recycle:

---
+++ SophiApp/SophiApp/Controls/ToggleButton.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SophiApp.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для ToggleButton.xaml
+    /// </summary>
+    public partial class ToggleButton : UserControl
+    {
+        public ToggleButton()
+        {
+            InitializeComponent();
+        }
+
+        public ICommand Command
+        {
+            get { return (ICommand)GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Command.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.Register("Command", typeof(ICommand), typeof(ToggleButton), new PropertyMetadata(default));
+
+
+        public object CommandParameter
+        {
+            get { return (object)GetValue(CommandParameterProperty); }
+            set { SetValue(CommandParameterProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for CommandParameter.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CommandParameterProperty =
+            DependencyProperty.Register("CommandParameter", typeof(object), typeof(ToggleButton), new PropertyMetadata(default));
+
+
+        public bool State
+        {
+            get { return (bool)GetValue(StateProperty); }
+            set { SetValue(StateProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for State.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty StateProperty =
+            DependencyProperty.Register("State", typeof(bool), typeof(ToggleButton), new PropertyMetadata(default));
+
+        public string TextOn
+        {
+            get { return (string)GetValue(TextOnProperty); }
+            set { SetValue(TextOnProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for TextOn.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextOnProperty =
+            DependencyProperty.Register("TextOn", typeof(string), typeof(ToggleButton), new PropertyMetadata(default));
+
+        public string TextOff
+        {
+            get { return (string)GetValue(TextOffProperty); }
+            set { SetValue(TextOffProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for TextOff.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextOffProperty =
+            DependencyProperty.Register("TextOff", typeof(string), typeof(ToggleButton), new PropertyMetadata(default));
+
+        private void BorderOff_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (State)
+            {
+                State = false;
+                Command?.Execute(CommandParameter);
+            }
+        }
+
+        private void BorderOn_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (State)
+            {
+                return;
+            }
+
+            State = true;
+            Command?.Execute(CommandParameter);
+        }
+    }
+}
