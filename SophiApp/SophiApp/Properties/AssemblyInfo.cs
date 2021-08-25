917988e923113f00ed9ac14551ac3c47ede25639
Inestic <50652175+Inestic@users.noreply.github.com>
2021-08-25 11:03:22 +0300

    Fixed texted elements header :bug:

--- SophiApp/SophiApp/Properties/AssemblyInfo.cs
+++ SophiApp/SophiApp/Properties/AssemblyInfo.cs
@@ -47,5 +47,5 @@ using System.Windows;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.0.18")]
+[assembly: AssemblyVersion("0.0.0.21")]
 [assembly: AssemblyFileVersion("0.0.0.0")]
