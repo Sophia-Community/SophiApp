66518428504e85da4dd879b37663edd268c8763f
Inestic <50652175+Inestic@users.noreply.github.com>
2021-08-26 17:07:09 +0300

    Added windows icon checkbox :tropical_fish:

--- SophiApp/SophiApp/Controls/IconWinCheckBox.xaml.cs
+++ SophiApp/SophiApp/Controls/IconWinCheckBox.xaml.cs
@@ -6,39 +6,35 @@ using System.Windows.Input;
 namespace SophiApp.Controls
 {
     /// <summary>
-    /// Логика взаимодействия для FolderIconCheckBox.xaml
+    /// Логика взаимодействия для IconWinCheckBox.xaml
     /// </summary>
-    public partial class FolderIconCheckBox : UserControl
+    public partial class IconWinCheckBox : UserControl
     {
-        private static new readonly RoutedEvent MouseEnterEvent = EventManager.RegisterRoutedEvent("MouseEnter", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(FolderIconCheckBox));
+        private static new readonly RoutedEvent MouseEnterEvent = EventManager.RegisterRoutedEvent("MouseEnter", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(IconWinCheckBox));
 
-        private static new readonly RoutedEvent MouseLeaveEvent = EventManager.RegisterRoutedEvent("MouseLeave", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(FolderIconCheckBox));
-
-        // Using a DependencyProperty as the backing store for CommandParameter.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty CommandParameterProperty =
-            DependencyProperty.Register("CommandParameter", typeof(object), typeof(FolderIconCheckBox), new PropertyMetadata(default));
+        private static new readonly RoutedEvent MouseLeaveEvent = EventManager.RegisterRoutedEvent("MouseLeave", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(IconWinCheckBox));
 
         // Using a DependencyProperty as the backing store for Command.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty CommandProperty =
-            DependencyProperty.Register("Command", typeof(ICommand), typeof(FolderIconCheckBox), new PropertyMetadata(default));
+            DependencyProperty.Register("Command", typeof(ICommand), typeof(IconWinCheckBox), new PropertyMetadata(default));
 
         // Using a DependencyProperty as the backing store for Description.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty DescriptionProperty =
-            DependencyProperty.Register("Description", typeof(string), typeof(FolderIconCheckBox), new PropertyMetadata(default));
+            DependencyProperty.Register("Description", typeof(string), typeof(IconWinCheckBox), new PropertyMetadata(default));
 
         // Using a DependencyProperty as the backing store for Header.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty HeaderProperty =
-            DependencyProperty.Register("Header", typeof(string), typeof(FolderIconCheckBox), new PropertyMetadata(default));
+            DependencyProperty.Register("Header", typeof(string), typeof(IconWinCheckBox), new PropertyMetadata(default));
 
         // Using a DependencyProperty as the backing store for Id.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty IdProperty =
-            DependencyProperty.Register("Id", typeof(int), typeof(FolderIconCheckBox), new PropertyMetadata(default));
+            DependencyProperty.Register("Id", typeof(int), typeof(IconWinCheckBox), new PropertyMetadata(default));
 
         // Using a DependencyProperty as the backing store for IsChecked.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty IsCheckedProperty =
-            DependencyProperty.Register("IsChecked", typeof(bool), typeof(FolderIconCheckBox), new PropertyMetadata(default));
+            DependencyProperty.Register("IsChecked", typeof(bool), typeof(IconWinCheckBox), new PropertyMetadata(default));
 
-        public FolderIconCheckBox()
+        public IconWinCheckBox()
         {
             InitializeComponent();
         }
@@ -59,12 +55,6 @@ namespace SophiApp.Controls
         {
             get { return (ICommand)GetValue(CommandProperty); }
             set { SetValue(CommandProperty, value); }
-        }
-
-        public object CommandParameter
-        {
-            get { return (object)GetValue(CommandParameterProperty); }
-            set { SetValue(CommandParameterProperty, value); }
         }
 
         public string Description
@@ -95,10 +85,10 @@ namespace SophiApp.Controls
 
         private void ContextMenu_HeaderCopyClick(object sender, RoutedEventArgs e) => ClipboardHelper.CopyText(Header);
 
-        private void FolderIconCheckBox_MouseEnter(object sender, MouseEventArgs e) => RaiseEvent(new RoutedEventArgs(MouseEnterEvent) { Source = Description });
+        private void IconWinCheckBox_MouseEnter(object sender, MouseEventArgs e) => RaiseEvent(new RoutedEventArgs(MouseEnterEvent) { Source = Description });
 
-        private void FolderIconCheckBox_MouseLeave(object sender, MouseEventArgs e) => RaiseEvent(new RoutedEventArgs(MouseLeaveEvent));
+        private void IconWinCheckBox_MouseLeave(object sender, MouseEventArgs e) => RaiseEvent(new RoutedEventArgs(MouseLeaveEvent));
 
-        private void FolderIconCheckBox_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) => Command?.Execute(CommandParameter);
+        private void IconWinCheckBox_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) => Command?.Execute(DataContext);
     }
 }
---
+++ SophiApp/SophiApp/Models/IconWinCheckBox.cs
@@ -0,0 +1,11 @@
+﻿using SophiApp.Commons;
+
+namespace SophiApp.Models
+{
+    internal class IconWinCheckBox : TextedElement
+    {
+        public IconWinCheckBox(TextedElementDTO dataObject) : base(dataObject)
+        {
+        }
+    }
+}
