2c05fb815e15ce754fb0254ff6cd011e00f3840e
Dmitry Nefedov <nefedovds@ya.ru>
2021-09-16 14:53:44 +0300

    Update Uncomment-AppMethods.ps1

--- SophiApp/Scripts/Uncomment-AppMethods.ps1
+++ SophiApp/Scripts/Uncomment-AppMethods.ps1
@@ -1,15 +1,15 @@
-﻿<#
+<#
 	.SYNOPSIS
-	Comments for testing in .cs files are removed.
+	Comments for testing in .cs files are removed
 
 	.INPUTS
-	None.
+	None
 
 	.OUTPUTS
-	Comments in .cs files are removed.
+	Comments in .cs files are removed
 
 	.NOTES
-	Designed for GitHub Actions.
+	Designed for GitHub Actions
 	
 	.LINK
 	https://github.com/Inestic
@@ -23,22 +23,26 @@
 	Copyright (c) 2021 Inestic
 #>
 Clear-Host
-$MethodsCS = "{0}\{1}"-f (Split-Path -Path $PSScriptRoot -Parent), "SophiApp\ViewModels\Methods.cs"
+
+$MethodsCS = "{0}\{1}" -f (Split-Path -Path $PSScriptRoot -Parent), "SophiApp\ViewModels\Methods.cs"
 $CommentPattern = "//"
 $UpdatePattern = "//await UpdateIsAvailableAsync()"
-Write-Host "Path to Methods.cs: ""$MethodsCS"""
+
+Write-Host "`nPath to Methods.cs: ""$MethodsCS"""
 
 if (Test-Path -Path $MethodsCS)
 {
-	Write-Host "Methods.cs found"
+	Write-Host "`nMethods.cs found"
+
 	$MethodsContent = Get-Content -Path $MethodsCS
 	$LineNumber = ($MethodsContent | Select-String -Pattern $UpdatePattern | Select-Object -Last 1).LineNumber
 	$FormatedString = $MethodsContent[$LineNumber - 1]
 	$MethodsContent[$LineNumber - 1] = $FormatedString.Replace($CommentPattern, $null)	
 	Set-Content -Path $MethodsCS -Value $MethodsContent -Confirm:$false -Encoding UTF8 -Force
-	Write-Host "File ""$MethodsCS"" saved"
+
+	Write-Host "`nFile ""$MethodsCS"" saved"
 }
 else
 {
-	Write-Host "Methods.cs not found"
+	Write-Host "`nMethods.cs not found"
 }
