5d301e234e1509781da5e89058d378e89cd26eb5
Dmitry Nefedov <nefedovds@ya.ru>
2021-09-16 14:52:28 +0300

    Update Set-DebugMode.ps1

--- SophiApp/Scripts/Set-DebugMode.ps1
+++ SophiApp/Scripts/Set-DebugMode.ps1
@@ -23,21 +23,24 @@
 	Copyright (c) 2021 Inestic
 #>
 
-$MethodsCS = "{0}\{1}"-f (Split-Path -Path $PSScriptRoot -Parent), "SophiApp\ViewModels\Methods.cs"
+$MethodsCS = "{0}\{1}" -f (Split-Path -Path $PSScriptRoot -Parent), "SophiApp\ViewModels\Methods.cs"
 $DebugModePattern = "DebugMode = true"
-Write-Host "Path to Methods.cs: ""$MethodsCS"""
+
+Write-Host "`nPath to Methods.cs: ""$MethodsCS"""
 
 if (Test-Path -Path $MethodsCS)
 {
-	Write-Host "Methods.cs found"
+	Write-Host "`nMethods.cs found"
+
 	$MethodsContent = Get-Content -Path $MethodsCS
 	$LineNumber = ($MethodsContent | Select-String -Pattern $DebugModePattern | Select-Object -Last 1).LineNumber
 	$FormatedString = $MethodsContent[$LineNumber - 1]
 	$MethodsContent[$LineNumber - 1] = $FormatedString.Replace("true", "false")	
 	Set-Content -Path $MethodsCS -Value $MethodsContent -Confirm:$false -Encoding UTF8 -Force
-	Write-Host "File ""$MethodsCS"" saved"
+
+	Write-Host "`nFile ""$MethodsCS"" saved"
 }
 else
 {
-	Write-Host "Methods.cs not found"
+	Write-Host "`nMethods.cs not found"
 }
