fa6f67212f2bc00c7b37f5a1e5dc3b8af09283ac
Dmitry Nefedov <nefedovds@ya.ru>
2021-12-30 12:52:36 +0300

    Update AppHelper.cs

--- SophiApp/SophiApp/Helpers/AppHelper.cs
+++ SophiApp/SophiApp/Helpers/AppHelper.cs
@@ -17,7 +17,7 @@ namespace SophiApp.Helpers
         private const string GITHUB_RELEASES_PAGE = "https://github.com/Sophia-Community/SophiApp/releases";
         private const string GITHUB_REPO = "https://github.com/Sophia-Community/SophiApp";
         private const string LEADER_LINK = "https://github.com/farag2";
-        private const string LOGOTYPE_CREATOR_LINK = "https://www.linkedin.com/in/nataly/";
+        private const string LOGOTYPE_CREATOR_LINK = "https://www.linkedin.com/mwlite/in/наталия-гуменюк-ba4a04161";
         private const string USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36 Edg/90.0.818.49";
         
         private static readonly string APP_NAME = Assembly.GetExecutingAssembly().GetName().Name;
@@ -28,6 +28,5 @@ namespace SophiApp.Helpers
         public static string GitHubRepo => GITHUB_REPO;
         public static string LeaderLink => LEADER_LINK;
         public static string LogotypeCreatorLink => LOGOTYPE_CREATOR_LINK;
-        public static Version Version => Assembly.GetExecutingAssembly().GetName().Version;
-    }
+        public static Version Version => Assembly.GetExecutingAssembly().GetName().Version;    }
 }
