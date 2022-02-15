e6b4bd9eb51fed955d593b28c2e43a9b1ef72f17
Inestic <50652175+Inestic@users.noreply.github.com>
2022-02-15 11:13:38 +0300

    Update Set-ReleaseTag.ps1

--- SophiApp/Scripts/Set-ReleaseTag.ps1
+++ SophiApp/Scripts/Set-ReleaseTag.ps1
@@ -22,30 +22,33 @@
 
 	Copyright (c) 2021 Inestic
 #>
-$ReleaseTag = $args[0].Split("/") | Select-Object -Last 1
-$AssemblyInfo = "{0}\{1}" -f (Split-Path -Path $PSScriptRoot -Parent), "SophiApp\Properties\AssemblyInfo.cs"
-$AssemblyPattern = "AssemblyVersion"
-$AssemblyFilePattern = "AssemblyFileVersion"
-$AssemblyString = '[assembly: AssemblyVersion("{0}")]' -f $ReleaseTag
-$AssemblyFileString = '[assembly: AssemblyFileVersion("{0}")]' -f $ReleaseTag
-
-Write-Host "`nRelease tag: ""$ReleaseTag"""
-Write-Host "`nPath to AssemblyInfo.cs: ""$AssemblyInfo"""
-
-if (Test-Path -Path $AssemblyInfo)
-{
-	Write-Host "`nAssemblyInfo.cs found"
-
-	$AssemblyContent = Get-Content -Path $AssemblyInfo
-	$AssemblyLineNumber = ($AssemblyContent | Select-String -Pattern $AssemblyPattern | Select-Object -Last 1).LineNumber
-	$AssemblyFileLineNumber = ($AssemblyContent | Select-String -Pattern $AssemblyFilePattern).LineNumber
-	$AssemblyContent[$AssemblyLineNumber - 1] = $AssemblyString
-	$AssemblyContent[$AssemblyFileLineNumber - 1] = $AssemblyFileString
-	Set-Content -Path $AssemblyInfo -Value $AssemblyContent -Confirm:$false -Encoding UTF8 -Force
-
-	Write-Host "`nFile ""$AssemblyInfo"" saved"
-}
-else
-{
-	Write-Host "`nAssemblyInfo.cs not found"
-}
+$ReleaseApi = "https://api.github.com/repos/Sophia-Community/SophiApp/releases"
+$ReleaseResponse = (Invoke-WebRequest -Uri $ReleaseApi | ConvertFrom-Json)[0]
+Write-Host "`nRelease: $ReleaseResponse"
+#$ReleaseTag = $args[0].Split("/") | Select-Object -Last 1
+#$AssemblyInfo = "{0}\{1}" -f (Split-Path -Path $PSScriptRoot -Parent), "SophiApp\Properties\AssemblyInfo.cs"
+#$AssemblyPattern = "AssemblyVersion"
+#$AssemblyFilePattern = "AssemblyFileVersion"
+#$AssemblyString = '[assembly: AssemblyVersion("{0}")]' -f $ReleaseTag
+#$AssemblyFileString = '[assembly: AssemblyFileVersion("{0}")]' -f $ReleaseTag
+#
+#Write-Host "`nRelease tag: ""$ReleaseTag"""
+#Write-Host "`nPath to AssemblyInfo.cs: ""$AssemblyInfo"""
+#
+#if (Test-Path -Path $AssemblyInfo)
+#{
+#	Write-Host "`nAssemblyInfo.cs found"
+#
+#	$AssemblyContent = Get-Content -Path $AssemblyInfo
+#	$AssemblyLineNumber = ($AssemblyContent | Select-String -Pattern $AssemblyPattern | Select-Object -Last 1).LineNumber
+#	$AssemblyFileLineNumber = ($AssemblyContent | Select-String -Pattern $AssemblyFilePattern).LineNumber
+#	$AssemblyContent[$AssemblyLineNumber - 1] = $AssemblyString
+#	$AssemblyContent[$AssemblyFileLineNumber - 1] = $AssemblyFileString
+#	Set-Content -Path $AssemblyInfo -Value $AssemblyContent -Confirm:$false -Encoding UTF8 -Force
+#
+#	Write-Host "`nFile ""$AssemblyInfo"" saved"
+#}
+#else
+#{
+#	Write-Host "`nAssemblyInfo.cs not found"
+#}
