e633938889453bf25fbf28209012821b27640123
Inestic <50652175+Inestic@users.noreply.github.com>
2022-03-31 16:42:18 +0300

    Fixed startup condition name :bug:

--- SophiApp/SophiApp/StartupConditions/DefenderNotCorruptedCondition.cs
+++ SophiApp/SophiApp/StartupConditions/DefenderNotCorruptedCondition.cs
@@ -4,12 +4,12 @@ using SophiApp.Interfaces;
 
 namespace SophiApp.StartupConditions
 {
-    internal class Win10TweakerDefenderBroken : IStartupCondition
+    internal class DefenderNotCorruptedCondition : IStartupCondition
     {
         private const string AME_WRONG_VERSION = "0.0.0.0";
 
         public bool HasProblem { get; set; }
-        public ConditionsTag Tag { get; set; } = ConditionsTag.Win10TweakerBrokeDefender;
+        public ConditionsTag Tag { get; set; } = ConditionsTag.DefenderCorrupted;
 
         public bool Invoke() => WmiHelper.HasExternalAntiVirus() ? HasProblem = false : WmiHelper.GetDefenderAMEngineVersion() == AME_WRONG_VERSION;
     }
