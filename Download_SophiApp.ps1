42633abc0a83dfdb2e52caefe68b0a3bda2ec4d3
Dmitry Nefedov <nefedovds@ya.ru>
2022-05-28 16:03:05 +0300

    Update Download_SophiApp.ps1

--- Download_SophiApp.ps1
+++ Download_SophiApp.ps1
@@ -6,7 +6,7 @@
 	irm app.sophi.app | iex
 #>
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
+cls
 $DownloadsFolder = Get-ItemPropertyValue -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "{374DE290-123F-4565-9164-39C4925E467B}"
 
 $Parameters = @{
@@ -50,12 +50,15 @@ if (-not ("WinAPI.ForegroundWindow" -as [type]))
 	Add-Type @SetForegroundWindow
 }
 
+Start-Sleep -Seconds 1
+
 Get-Process -Name explorer | Where-Object -FilterScript {$_.MainWindowTitle -eq "SophiApp"} | ForEach-Object -Process {
 	# Show window, if minimized
 	[WinAPI.ForegroundWindow]::ShowWindowAsync($_.MainWindowHandle, 5)
 
-	Start-Sleep -Seconds 3
-
 	# Force move the console window to the foreground
 	[WinAPI.ForegroundWindow]::SetForegroundWindow($_.MainWindowHandle)
 } | Out-Null
+
+Write-Information -MessageData "" -InformationAction Continue
+Write-Verbose -Message "Archive was expended to `"$DownloadsFolder\SophiApp`"" -Verbose
