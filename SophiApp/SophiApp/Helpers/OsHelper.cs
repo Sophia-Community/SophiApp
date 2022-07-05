d110d2eabd9daf80d88a962d978adac6e648b6a8
Dmitry Nefedov <nefedovds@ya.ru>
2022-07-05 16:46:06 +0300

    Update OsHelper.cs

--- SophiApp/SophiApp/Helpers/OsHelper.cs
+++ SophiApp/SophiApp/Helpers/OsHelper.cs
@@ -45,7 +45,7 @@ namespace SophiApp.Helpers
 
         internal const uint WIN10_MAX_SUPPORTED_BUILD = 19048;
         internal const uint WIN10_MIN_SUPPORTED_BUILD = 19041;
-        internal const uint WIN10_MIN_SUPPORTED_UBR = 1766;
+        internal const uint WIN10_MIN_SUPPORTED_UBR = 1706;
         internal const uint WIN11_INSIDER_BUILD_PATTERN = 22500;
         internal const uint WIN11_MIN_SUPPORTED_BUILD = 22000;
         internal const uint WIN11_MIN_SUPPORTED_INSIDER_BUILD = 22509;
