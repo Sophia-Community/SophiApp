4a5bb036e34de75b50482c1ba19731feea0d192f
Inestic <50652175+Inestic@users.noreply.github.com>
2022-07-11 21:00:23 +0300

    Fixed Hevc appx link :bug:

--- SophiApp/SophiApp/Helpers/StartupConditionsHelper.cs
+++ SophiApp/SophiApp/Helpers/StartupConditionsHelper.cs
@@ -29,7 +29,7 @@ namespace SophiApp.Helpers
             new OsVersionCondition(), new OsBuildVersionCondition(), new OsFilesNotCorruptedCondition(), new RebootRequiredCondition(),
             new SingleInstanceCondition(), new SingleAdminSessionCondition(), new Win10TweakerCondition(), new SycnexScriptCondition(),
             new DefenderWarningCondition(), new DefenderNotCorruptedCondition(), new SpoolerNotCorruptedCondition(),
-            new NewVersionCondition()
+            //new NewVersionCondition()
         };
 
         internal async Task CheckAsync()
