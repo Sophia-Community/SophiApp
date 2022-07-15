c805e0bba0d71a2b0c4e8fc40e7da3f7f37efa47
Inestic <50652175+Inestic@users.noreply.github.com>
2022-07-15 09:52:19 +0300

    Fixed OsBuildVersionCondition logic :bug:

--- SophiApp/SophiApp/StartupConditions/OsVersionCondition.cs
+++ SophiApp/SophiApp/StartupConditions/OsVersionCondition.cs
@@ -12,8 +12,26 @@ namespace SophiApp.Conditions
         public bool Invoke()
         {
             var build = OsHelper.GetBuild();
-            var hasProblem = build >= OsHelper.WIN11_MIN_SUPPORTED_BUILD || (build >= OsHelper.WIN10_MIN_SUPPORTED_BUILD & build <= OsHelper.WIN10_MAX_SUPPORTED_BUILD);
+            var ubr = OsHelper.GetUpdateBuildRevision();
+            var hasProblem = OsHelper.IsWindows11()
+                           ? CheckWin11Version(build, ubr)
+                           : CheckWin10Version(build);
+
             return HasProblem = hasProblem.Invert();
+        }
+
+        private bool CheckWin10Version(ushort build) => build >= OsHelper.WIN10_MIN_SUPPORTED_BUILD & build <= OsHelper.WIN10_MAX_SUPPORTED_BUILD;
+
+        private bool CheckWin11Version(ushort build, ushort ubr)
+        {
+            const uint win11SupportedVersion = 22000;
+            const uint win11SupportedUbr = 739;
+
+            return build < win11SupportedVersion
+                  ? false
+                  : build == win11SupportedVersion
+                          ? ubr >= win11SupportedUbr
+                          : true;
         }
     }
 }
