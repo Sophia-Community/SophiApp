a8ddbe281429c9a86128ec22d1603c19896fc6c7
Dmitry Nefedov <nefedovds@ya.ru>
2022-08-17 00:13:10 +0300

    Update AssemblyInfo.cs

--- SophiApp/SophiApp/Properties/AssemblyInfo.cs
+++ SophiApp/SophiApp/Properties/AssemblyInfo.cs
@@ -48,6 +48,6 @@ using System.Windows;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyVersion("0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0")]
 [assembly: NeutralResourcesLanguage("")]
