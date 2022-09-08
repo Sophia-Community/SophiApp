04cd38a0e3959ad908dcc9e0b4b1ace97aa707b0
Dmitry Nefedov <nefedovds@ya.ru>
2022-09-08 12:20:35 +0300

    Update Get-BinaryFiles.ps1
    Wrote SophiApp.yml' scripts

---
+++ SophiApp/Scripts/Copy-Dependencies.ps1
@@ -0,0 +1,5 @@
+$ReleaseBinDir = "$(Split-Path -Path $PSScriptRoot -Parent)\SophiApp\bin\Release\Bin"
+New-Item -Path (Split-Path -Path $ReleaseBinDir -Parent) -Name Bin -ItemType Directory -Force
+
+# Copy downloaded dependencies to the created Bin folder
+Get-ChildItem -Path "SophiApp\Binary" | Copy -Destination $ReleaseBinDir -Force
