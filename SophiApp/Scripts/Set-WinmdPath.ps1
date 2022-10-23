638bbbb876cdabd1c693d69071f030bded09efd4
Dmitry Nefedov <nefedovds@ya.ru>
2022-10-23 19:07:46 +0300

    Revert "Small changes"
    This reverts commit 0f21d3445ae50de12edfdb8df822d2a7ec766459.

--- SophiApp/Scripts/Set-WinmdPath.ps1
+++ SophiApp/Scripts/Set-WinmdPath.ps1
@@ -10,9 +10,12 @@
 
 	.NOTES
 	Designed for GitHub Actions
-
+	
 	.LINK
 	https://github.com/Inestic
+
+	.VERSION
+	v1.0.0
 
 	.DATE
 	02.11.2021
@@ -25,17 +28,17 @@ function Set-BinaryLink
 	[CmdletBinding()]
 	param
 	(
-		[Parameter(Mandatory=$True)]
-		[ValidateNotNull()]
-		[string]$CsProjPath,
+    	[Parameter(Mandatory=$True)]
+    	[ValidateNotNull()]
+    	[string]$CsProjPath,
+		
+    	[Parameter(Mandatory=$True)]
+    	[ValidateNotNull()]
+    	[string]$SearchPattern,
 		
 		[Parameter(Mandatory=$True)]
-		[ValidateNotNull()]
-		[string]$SearchPattern,
-		
-		[Parameter(Mandatory=$True)]
-		[ValidateNotNull()]
-		[string]$ChangePattern
+    	[ValidateNotNull()]
+    	[string]$ChangePattern
 	)
 	
 	process
