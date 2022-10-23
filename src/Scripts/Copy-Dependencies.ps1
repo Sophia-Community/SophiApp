c08f01e87cb91037a3a07abe378e02dd5ce30acf
Dmitry Nefedov <nefedovds@ya.ru>
2022-10-23 17:45:39 +0300

    Update Copy-Dependencies.ps1

--- src/Scripts/Copy-Dependencies.ps1
+++ src/Scripts/Copy-Dependencies.ps1
@@ -1,4 +1,4 @@
-$ReleaseBinDir = "$(Split-Path -Path $PSScriptRoot -Parent)\SophiApp\bin\Release\Bin"
+$ReleaseBinDir = "$(Split-Path -Path $PSScriptRoot -Parent)\src\bin\Release\Bin"
 New-Item -Path (Split-Path -Path $ReleaseBinDir -Parent) -Name Bin -ItemType Directory -Force
 
 # Copy downloaded dependencies to the created Bin folder
