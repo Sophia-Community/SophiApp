b92f2c55fc9bb7ee80b5430415f344c5a93927f0
Dmitry Nefedov <nefedovds@ya.ru>
2022-10-25 22:29:44 +0300

    Delete Uncomment-AppMethods.ps1

--- src/Scripts/Uncomment-AppMethods.ps1
+++
@@ -1,7 +0,0 @@
-# Remove comments in .cs files
-$MethodsContent = Get-Content -Path "src\SophiApp\Helpers\StartupConditionsHelper.cs"
-$LineNumber = ($MethodsContent | Select-String -Pattern "//new NewVersionCondition()" | Select-Object -Last 1).LineNumber
-$FormatedString = $MethodsContent[$LineNumber - 1]
-$MethodsContent[$LineNumber - 1] = $FormatedString.Replace("//", $null)
-Set-Content -Path "src\SophiApp\Helpers\StartupConditionsHelper.cs" -Value $MethodsContent -Confirm:$false -Encoding UTF8 -Force
-
