af929b7f9750bacb6db2710ec10f9c0e28379f4e
Dmitry Nefedov <nefedovds@ya.ru>
2022-10-25 22:31:15 +0300

    Delete Copy-Dependencies.ps1

--- src/Scripts/Copy-Dependencies.ps1
+++
@@ -1,3 +0,0 @@
-# Copy downloaded dependencies to the created Bin folder
-New-Item -Path src\SophiApp\bin\Release\Bin -ItemType Directory -Force
-Get-ChildItem -Path "src\Binary" | Copy -Destination src\SophiApp\bin\Release\Bin -Force
