26aa4863ebdec792838a2ce5383c7f5f9b4271ea
Inestic <50652175+Inestic@users.noreply.github.com>
2023-04-25 22:05:13 +0300

    Set up DI :boom:

--- src/SophiApp/App.xaml.cs
+++ src/SophiApp/App.xaml.cs
@@ -4,12 +4,33 @@
 
 namespace SophiApp
 {
+    using System;
     using System.Windows;
+    using Microsoft.Extensions.Hosting;
 
     /// <summary>
     /// Interaction logic for App.xaml.
     /// </summary>
     public partial class App : Application
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="App"/> class.
+        /// </summary>
+        public App()
+            : base()
+        {
+            Host = Microsoft.Extensions.Hosting.Host
+            .CreateDefaultBuilder()
+            .UseContentRoot(AppContext.BaseDirectory)
+            .ConfigureServices((context, services) =>
+            {
+            })
+            .Build();
+        }
+
+        /// <summary>
+        /// Gets a <see cref="IHost"/>.
+        /// </summary>
+        public IHost Host { get; init; }
     }
 }
