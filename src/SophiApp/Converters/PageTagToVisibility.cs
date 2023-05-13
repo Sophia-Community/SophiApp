c3a0008c43e19b2cc73258dedc94f566d6b95f7c
Inestic <50652175+Inestic@users.noreply.github.com>
2023-05-13 10:06:01 +0300

    Added category button marker

---
+++ src/SophiApp/Converters/PageTagToVisibility.cs
@@ -0,0 +1,25 @@
+﻿// <copyright file="PageTagToVisibility.cs" company="Team Sophia">
+// Copyright (c) Team Sophia. All rights reserved.
+// </copyright>
+
+namespace SophiApp.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows;
+    using System.Windows.Data;
+
+    /// <summary>
+    /// Defines visibility depending on the active page.
+    /// </summary>
+    public class PageTagToVisibility : IMultiValueConverter
+    {
+        /// <inheritdoc/>
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+            => values[0].ToString() == values[1].ToString() ? Visibility.Visible : Visibility.Collapsed;
+
+        /// <inheritdoc/>
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+            => new object[0];
+    }
+}
