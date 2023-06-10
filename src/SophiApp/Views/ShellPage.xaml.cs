31c887070e2620a28d4ee2f903ab513b0f1a5daa
Inestic <50652175+Inestic@users.noreply.github.com>
2023-06-10 07:14:21 +0300

    Added Navigation View selector color

--- src/SophiApp/Views/ShellPage.xaml.cs
+++ src/SophiApp/Views/ShellPage.xaml.cs
@@ -29,8 +29,6 @@ public sealed partial class ShellPage : Page
         ViewModel.NavigationService.Frame = NavigationFrame;
         ViewModel.NavigationViewService.Initialize(NavigationViewControl);
 
-        // TODO: Set the title bar icon by updating /Assets/WindowIcon.ico.
-        // A custom title bar is required for full window theme and Mica support.
         // https://docs.microsoft.com/windows/apps/develop/title-bar?tabs=winui3#full-customization
         App.MainWindow.ExtendsContentIntoTitleBar = true;
         App.MainWindow.SetTitleBar(AppTitleBar);
