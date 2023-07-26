3e0fb62cf4fdc938e1f2117429e9505ce032ae30
Inestic <50652175+Inestic@users.noreply.github.com>
2023-07-26 22:09:29 +0300

    Revert "Fixed Windows 11 build :construction:"
    This reverts commit dc0386278149f12b765e32e494f68da21df7b628.

--- src/SophiApp/Helpers/OsHelper.cs
+++ src/SophiApp/Helpers/OsHelper.cs
@@ -33,8 +33,7 @@ namespace SophiApp.Helpers
         private const string UBR = "UBR";
         private const string WIN_ENTERPRISE_G = "EnterpriseG";
         private const uint WIN11_BUILD_NUMBER = 22;
-        private const uint WIN11_INSIDER_BUILD_NUMBER_23 = 23;
-        private const uint WIN11_INSIDER_BUILD_NUMBER_25 = 25;
+        private const uint WIN11_INSIDER_BUILD_NUMBER = 23;
         private const string WINLOGON_PATH = @"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon";
         private const int WM_SETTINGCHANGE = 0x1a;
         private static readonly IntPtr hWnd = new IntPtr(65535);
@@ -105,7 +104,7 @@ namespace SophiApp.Helpers
         internal static bool IsWindows11()
         {
             var build = GetBuild() / 1000;
-            return build == WIN11_BUILD_NUMBER || build == WIN11_INSIDER_BUILD_NUMBER_23 || build == WIN11_INSIDER_BUILD_NUMBER_25;
+            return build == WIN11_BUILD_NUMBER || build == WIN11_INSIDER_BUILD_NUMBER;
         }
 
         internal static void SafelyRestartExplorerProcess()
