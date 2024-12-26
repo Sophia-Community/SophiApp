5dcef4de7d2844d89efa2bbc7ce318aa3ed14b3f
Rich Lander <rlander@microsoft.com>
2024-12-26 14:13:46 -0800

    Update src/SophiApp/Customisations/CustomisationOs.cs
    Co-authored-by: Kalle Olavi Niemitalo <kon@iki.fi>

--- src/SophiApp/Customisations/CustomisationOs.cs
+++ src/SophiApp/Customisations/CustomisationOs.cs
@@ -960,7 +960,7 @@ namespace SophiApp.Customisations
         public static void _354(bool _)
         {
             var temp = Environment.GetEnvironmentVariable(TEMP);
-            var cloudNet6 = WebHelper.GetJsonResponse<MsNetDto>(@"https://builds.dotnet.microsoft.com.blob.core.windows.net/dotnet/release-metadata/6.0/releases.json");
+            var cloudNet6 = WebHelper.GetJsonResponse<MsNetDto>(@"https://builds.dotnet.microsoft.com/dotnet/release-metadata/6.0/releases.json");
             var latestRelease = cloudNet6.Releases.Where(release => release.ReleaseVersion == $"{cloudNet6.LatestRelease}").First();
             var latestRuntime = latestRelease.WindowsDesktop.Files.Where(file => file.Name == "windowsdesktop-runtime-win-x86.exe").First();
             var installer = $@"{temp}\{latestRuntime.Url.Substring(latestRuntime.Url.LastIndexOf('/') + 1)}";
